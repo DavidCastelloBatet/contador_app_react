40aa811c86515ca59b692d4dca9e49d7c84a0b92
David <davidcastellobatet@gmail.com>
2021-09-28 01:47:26 +0200

    onClick + events

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,24 @@ import PropTypes from 'prop-types';
 
 
 const CounterApp = ({ value }) =>{
+
+  // handleAdd
+  const handleAdd = () => {
+    return console.log('click')
+  }
+  // handleAdd event (e). La e es un objeto con la información del evento
+  // de react. Tiene un monton de propiedades.
+  const handleAddEvent = (e) => {
+    return console.log(e)
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{ value }</h2>
+      { /* evento onClick*/}
+      <button onClick={ handleAdd }>+ 1</button> 
+      <button onClick={ handleAddEvent }>Event</button> 
     </>
   )
 }
