8c0e7e9fb937a8437d1763bfa3150e3174e702a5
David <davidcastellobatet@gmail.com>
2021-09-28 00:15:16 +0200

    configuracion proptypes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,8 @@
 //import { Fragment } from "react";
+import PropTypes from 'prop-types';
 
 
-const PrimeraApp = ( { saludo2 = 'Por defecto' } ) => {
+const PrimeraApp = ( { saludo2 } ) => {
   const nombre = 'David'
   const numeros = [1,2,3,4,5]
   const saludo = {
@@ -18,7 +19,14 @@ const PrimeraApp = ( { saludo2 = 'Por defecto' } ) => {
     </>
   )
 }
-
+// Las PropTypes de React nos pemiten verificar los tipos de las props 
+// de nuestros componentes. De esta forma, en desarollo, podemos saber 
+// si estamos pasándole al componente los tipos de datos correctos.
+PrimeraApp.propTypes = {
+  //saludo2: PropTypes.string,
+  // Con el .isRequired hacemos que la prop sea obligatoria
+  saludo2: PropTypes.string.isRequired,
+}
 
 export default PrimeraApp;
 
